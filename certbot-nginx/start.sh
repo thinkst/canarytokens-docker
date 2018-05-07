c0c6c017c134aeabeb42f2d3a6e5b9685cf17f1b
jay <jason@thinkst.com>
2018-05-07 11:17:13 +0200

    Add needed certbot-nginx directory and contents

---
+++ certbot-nginx/start.sh
@@ -0,0 +1,12 @@
+echo  "----------------------------------------------------------------"
+echo  "Starting nginx and lets encrypt setup using"
+echo  "Domain : $MY_DOMAIN_NAME"
+echo  "Email  : $EMAIL_ADDRESS"
+echo  "----------------------------------------------------------------"
+sed -i "s/___my.example.com___/$MY_DOMAIN_NAME/g" /etc/nginx/nginx.conf
+nginx
+sleep 5
+./certbot-auto --nginx -d $MY_DOMAIN_NAME -d www.$MY_DOMAIN_NAME --text --agree-tos --email $EMAIL_ADDRESS -v -n
+nginx -s stop
+sleep 3
+nginx -g "daemon off;"
