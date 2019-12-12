08c2970c7de9bbbb57eba1da384fd9a5ca3e8cad
Nicholas Rohrbeck <nick@thinkst.com>
2019-12-12 13:31:13 +0200

    Prevent certbot from upgrading and creating redirects
    Summary:
    With a latest change to certbot-auto, it would alter our `nginx.conf` file
    to the point that it would 404 non-ssl traffic. This is required for our http
    tokens as we don't enforce https on them.
    
    Adding the `--no-redirect` flag tells certbot-auto to not create the redirects.
    
    In addition, we've added the `--no-self-upgrade` flag so that we don't get suprise
    breakages going forward.
    
    Test Plan: We tested this while fixing the live instance.
    
    Reviewers: azhar
    
    Reviewed By: azhar
    
    Differential Revision: http://phabricator.thinkst.com:9000/D2309

--- certbot-nginx/start.sh
+++ certbot-nginx/start.sh
@@ -6,7 +6,7 @@ echo  "----------------------------------------------------------------"
 sed -i "s/___my.example.com___/$MY_DOMAIN_NAME/g" /etc/nginx/nginx.conf
 nginx
 sleep 5
-./certbot-auto --nginx -d $MY_DOMAIN_NAME -d www.$MY_DOMAIN_NAME --text --agree-tos --email $EMAIL_ADDRESS -v -n
+./certbot-auto --nginx -d $MY_DOMAIN_NAME -d www.$MY_DOMAIN_NAME --text --agree-tos --no-self-upgrade --no-redirect --email $EMAIL_ADDRESS -v -n
 nginx -s stop
 sleep 3
 nginx -g "daemon off;"
