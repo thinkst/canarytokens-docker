611fbaa58f627446b06584c15c96b9530782cb46
Jason Bissict <jason@thinkst.com>
2021-08-24 08:19:43 +0200

    Fix start.sh to report multiple domains if used

--- certbot-nginx/start.sh
+++ certbot-nginx/start.sh
@@ -1,20 +1,21 @@
 echo  "----------------------------------------------------------------"
 echo  "Starting nginx and lets encrypt setup using"
-echo  "Domain : $MY_DOMAIN_NAME"
-echo  "Email  : $EMAIL_ADDRESS"
-echo  "----------------------------------------------------------------"
 _args=""
 _server_names=""
 if [ "x${MY_DOMAIN_NAME}" != "x" ]; then
+    echo  "Domain : $MY_DOMAIN_NAME"
     _args=" -d ${MY_DOMAIN_NAME} -d www.${MY_DOMAIN_NAME}"
     _server_names="${MY_DOMAIN_NAME} www.${MY_DOMAIN_NAME} "
 fi
 if [ "x${MY_DOMAIN_NAMES}" != "x" ]; then
+    echo  "Domains : $MY_DOMAIN_NAMES"
     for domain in $MY_DOMAIN_NAMES; do
         _args="${_args} -d ${domain}"
         _server_names="${_server_names} ${domain}"
     done
 fi
+echo  "Email  : $EMAIL_ADDRESS"
+echo  "----------------------------------------------------------------"
 sed -i "s/___server_names___/$_server_names/g" /etc/nginx/nginx.conf
 nginx
 sleep 5
